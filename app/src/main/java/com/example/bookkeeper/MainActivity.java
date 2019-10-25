4a3a68bf9d2074541aefba2fd33fb51a1b02fea2
Edgar <esanchez.cse20@gmail.com>
2019-10-25 00:52:45 -0500

    Created a basic sign in and set up FireBase

--- app/src/main/java/com/example/bookkeeper/MainActivity.java
+++ app/src/main/java/com/example/bookkeeper/MainActivity.java
@@ -5,18 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    String email, password;
+
+    EditText editEmail;
+    EditText editPassword;
+
+    Button logInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get input from text boxes.
+        editEmail = (EditText) findViewById(R.id.emailInput);
+        editPassword = (EditText) findViewById(R.id.passwordInput);
+
+        logInButton = (Button) findViewById(R.id.logInBTN);
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = editEmail.getText().toString();
+                password = editPassword.getText().toString();
+
+                sendLogIn(email, password);
+            }
+        });
     }
 
-    public void sendLogIn(View view) {
-        Intent intent1 = new Intent(this, mainPage.class);
-        startActivity(intent1);
+
+    public void sendLogIn(String userEmail, String userPassword) {
+        if ((userEmail.equals("admin")) && (userPassword.equals("password"))){
+            Intent intent1 = new Intent(this, mainPage.class);
+            startActivity(intent1);
+        } else{
+
+        }
     }
 
     public void sendSignUp(View view) {
